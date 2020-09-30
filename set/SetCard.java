7f2ce34ff3f76ac614cd1361e2eac90e155e5a13
Anna Jarman <ajarman@carroll.edu>
2020-09-30 11:43:56 -0600

    Added constructor and displayCard method to Card class

--- set/SetCard.java
+++ set/SetCard.java
@@ -6,5 +6,18 @@ public class SetCard {
 	private String color;
 	private String fill;
 	private String shape;
+	
+	public SetCard(int number,String color,String fill,String shape)
+	{
+		this.number = number;
+		this.color = color;
+		this.fill = fill;
+		this.shape = shape;
+	}
+	
+	public void displayCard(String picFileName,int xCoord,int yCoord)
+	{
+		
+	}
 
 }
